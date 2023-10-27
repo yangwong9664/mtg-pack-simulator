4232eee3bf02a8e7a331047a53730bc5913f56a4
Yang Wong <20066247+yangwong9664@users.noreply.github.com>
2023-10-27 16:19:56 +0100

    random pack generation

--- src/main/scala/connectors/MtgApiConnector.scala
+++ src/main/scala/connectors/MtgApiConnector.scala
@@ -1,14 +1,21 @@
 package connectors
-
 import cats.effect.IO
 import com.typesafe.config.Config
-import models.external.BoosterPackResponse
+import models.external.{BoosterPackResponse, ScryfallCard}
 import org.http4s.client.Client
+
+import java.net.URLEncoder
 
 class MtgApiConnector(client: Client[IO], config: Config) {
 
   def generateBoosterPack(boosterId: String): IO[BoosterPackResponse] = {
     val request = client.expect[BoosterPackResponse](s"${config.getString("mtg")}/sets/$boosterId/booster")
+    request
+  }
+
+  def getRandomCard(rarity: String, set: String): IO[ScryfallCard] = {
+    val uri     = "?q=" + URLEncoder.encode(s"rarity:$rarity e:$set", "UTF-8")
+    val request = client.expect[ScryfallCard](s"${config.getString("mtg")}/cards/random$uri")
     request
   }
 }
--- src/main/scala/services/PackService.scala
+++ src/main/scala/services/PackService.scala
@@ -4,17 +4,49 @@ import cats.effect.IO
 import com.typesafe.scalalogging.StrictLogging
 import connectors.MtgApiConnector
 import models.MtgPackRequest
+import models.external.{BoosterPackResponse, ScryfallCard}
+import money.GBP
 import models.external.BoosterPackResponse
+import models.external.Prices._
+import models.external.ImageUris._
+import models.external.ScryfallCard
+import org.http4s.client.Client
 
+import java.net.URLEncoder
+import money._
 object PackService {
 
   def apply(mtgApiConnector: MtgApiConnector): PackService = new PackService with StrictLogging {
 
     override def generateBoosterPack(mtgPackRequest: MtgPackRequest): IO[BoosterPackResponse] =
       mtgApiConnector.generateBoosterPack(mtgPackRequest.boosterId)
+
+    override def getRandomCard(rarity: String, set: String): IO[ScryfallCard] =
+      mtgApiConnector.getRandomCard(rarity, set).map(usdToPounds)
+  }
+
+  private def usdToPounds(scryfallCard: ScryfallCard): ScryfallCard = {
+    def convert(usd: String) = {
+      val conversion: Conversion        = Map((USD, GBP) -> 0.63)
+      implicit val converter: Converter = Converter(conversion)
+      val amount                        = Money(usd.toDouble, USD) to GBP
+
+      f"${amount.amount}%.2f"
+    }
+
+    scryfallCard.copy(
+      prices = scryfallCard.prices.copy(
+        usd = convert(scryfallCard.prices.usd),
+        usd_foil = scryfallCard.prices.usd_foil.fold(None: Option[String]) { c =>
+          Some(convert(c))
+        }
+      )
+    )
   }
 }
 
 trait PackService {
   def generateBoosterPack(mtgPackRequest: MtgPackRequest): IO[BoosterPackResponse]
+  def getRandomCard(rarity: String, set: String): IO[ScryfallCard]
+
 }
